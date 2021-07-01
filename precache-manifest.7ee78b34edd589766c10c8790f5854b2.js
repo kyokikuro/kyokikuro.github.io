dbb652fbebd2390f4110e002bb46111bf0e7d0b1
ARMY <panuc129@syner.postbox.in.th>
2021-07-01 20:10:10 +0700

    Updates

---
+++ precache-manifest.7ee78b34edd589766c10c8790f5854b2.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f5da2c9d2a9dc45f28bd2317278039d",
+    "url": "/kyokikuro.github.io/index.html"
+  },
+  {
+    "revision": "8d6c9548550c97e064ce",
+    "url": "/kyokikuro.github.io/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "035a8b5e8f0e4afcd63f",
+    "url": "/kyokikuro.github.io/static/css/main.8c16403e.chunk.css"
+  },
+  {
+    "revision": "8d6c9548550c97e064ce",
+    "url": "/kyokikuro.github.io/static/js/2.8f7c804a.chunk.js"
+  },
+  {
+    "revision": "035a8b5e8f0e4afcd63f",
+    "url": "/kyokikuro.github.io/static/js/main.aa7a69fa.chunk.js"
+  },
+  {
+    "revision": "5b1ceab15176642ffae3",
+    "url": "/kyokikuro.github.io/static/js/runtime~main.0dac052d.js"
+  },
+  {
+    "revision": "e53c691e570a27ae345181c1da6e83be",
+    "url": "/kyokikuro.github.io/static/media/1.e53c691e.jpg"
+  },
+  {
+    "revision": "1824d5ef634e6dd04c294a14f4b91605",
+    "url": "/kyokikuro.github.io/static/media/10.1824d5ef.jpg"
+  },
+  {
+    "revision": "817aae654977dce0f663d60a812bd058",
+    "url": "/kyokikuro.github.io/static/media/11.817aae65.jpg"
+  },
+  {
+    "revision": "bc8b726f681fbfe3eae2abc73cd56aaa",
+    "url": "/kyokikuro.github.io/static/media/12.bc8b726f.jpg"
+  },
+  {
+    "revision": "0ce3f4e501f6dc83275025e56669a7d1",
+    "url": "/kyokikuro.github.io/static/media/13.0ce3f4e5.jpg"
+  },
+  {
+    "revision": "e1ac112e72052c71288e7708c9ceb25e",
+    "url": "/kyokikuro.github.io/static/media/14.e1ac112e.jpg"
+  },
+  {
+    "revision": "efa952c82bb5d9b3cf0169412b855f00",
+    "url": "/kyokikuro.github.io/static/media/15.efa952c8.jpg"
+  },
+  {
+    "revision": "c65cc098c4fb996e1a12addff4eff052",
+    "url": "/kyokikuro.github.io/static/media/16.c65cc098.jpg"
+  },
+  {
+    "revision": "32f401f9dd4f5da222f8e22b37ec3ba5",
+    "url": "/kyokikuro.github.io/static/media/17.32f401f9.jpg"
+  },
+  {
+    "revision": "cb5d45f317b57f8a808a4b6ea5390e99",
+    "url": "/kyokikuro.github.io/static/media/18.cb5d45f3.jpg"
+  },
+  {
+    "revision": "2fc8cccb42568244f79eac8102d83ae4",
+    "url": "/kyokikuro.github.io/static/media/2.2fc8cccb.jpg"
+  },
+  {
+    "revision": "5384755242e50b83fa22bf274d1c225c",
+    "url": "/kyokikuro.github.io/static/media/3.53847552.jpg"
+  },
+  {
+    "revision": "691bfca003c5b5e4662cc27cb250dd3b",
+    "url": "/kyokikuro.github.io/static/media/4.691bfca0.jpg"
+  },
+  {
+    "revision": "9cdf68c29d47bbc34e1126c8b71e57a5",
+    "url": "/kyokikuro.github.io/static/media/5.9cdf68c2.jpg"
+  },
+  {
+    "revision": "3ba389a8ca77182025ac1949fd86683c",
+    "url": "/kyokikuro.github.io/static/media/6.3ba389a8.jpg"
+  },
+  {
+    "revision": "b5c1508dbb523a6ff2a877fe99ff13de",
+    "url": "/kyokikuro.github.io/static/media/7.b5c1508d.jpg"
+  },
+  {
+    "revision": "385298f482f3dbcb502be7bdf9e14fd7",
+    "url": "/kyokikuro.github.io/static/media/8.385298f4.jpg"
+  },
+  {
+    "revision": "825d3672cc0dbbd903208bd04954bd1b",
+    "url": "/kyokikuro.github.io/static/media/9.825d3672.jpg"
+  }
+]);
